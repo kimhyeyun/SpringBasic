40c701a6ca0650524c3897bf4493dad6b5fd3030
Kimhyeyun <cqqwer@gmail.com>
2021-03-11 20:29:19 +0900

    스프링 @AOP 실습

--- spring-petclinic-main/src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
+++ spring-petclinic-main/src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
@@ -96,6 +96,7 @@ class OwnerController {
 	}
 
 	@PostMapping("/owners/new")
+	@LogExecutionTime
 	public String processCreationForm(@Valid Owner owner, BindingResult result) {
 		if (result.hasErrors()) {
 			return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
@@ -107,12 +108,14 @@ class OwnerController {
 	}
 
 	@GetMapping("/owners/find")
+	@LogExecutionTime
 	public String initFindForm(Map<String, Object> model) {
 		model.put("owner", new Owner());
 		return "owners/findOwners";
 	}
 
 	@GetMapping("/owners")
+	@LogExecutionTime
 	public String processFindForm(Owner owner, BindingResult result, Map<String, Object> model) {
 
 		// allow parameterless GET request for /owners to return all records
@@ -141,6 +144,7 @@ class OwnerController {
 	}
 
 	@GetMapping("/owners/{ownerId}/edit")
+	@LogExecutionTime
 	public String initUpdateOwnerForm(@PathVariable("ownerId") int ownerId, Model model) {
 		Owner owner = this.owners.findById(ownerId);
 		model.addAttribute(owner);
@@ -148,6 +152,7 @@ class OwnerController {
 	}
 
 	@PostMapping("/owners/{ownerId}/edit")
+	@LogExecutionTime
 	public String processUpdateOwnerForm(@Valid Owner owner, BindingResult result,
 			@PathVariable("ownerId") int ownerId) {
 		if (result.hasErrors()) {
@@ -166,6 +171,7 @@ class OwnerController {
 	 * @return a ModelMap with the model attributes for the view
 	 */
 	@GetMapping("/owners/{ownerId}")
+	@LogExecutionTime
 	public ModelAndView showOwner(@PathVariable("ownerId") int ownerId) {
 		ModelAndView mav = new ModelAndView("owners/ownerDetails");
 		Owner owner = this.owners.findById(ownerId);
--- spring-petclinic-main/src/main/java/org/springframework/samples/petclinic/proxy/Cash.java
+++ spring-petclinic-main/src/main/java/org/springframework/samples/petclinic/proxy/Cash.java
@@ -3,6 +3,6 @@ package org.springframework.samples.petclinic.proxy;
 public class Cash implements Payment{
 	@Override
 	public void pay(int amount) {
-		System.out.println(amount + "  현금");
+		System.out.println(amount + " 현금");
 	}
 }
